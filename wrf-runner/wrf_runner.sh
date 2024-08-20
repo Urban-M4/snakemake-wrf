cb793f51a2e9cb73887bef85420489c3e5e6c865
Peter Kalverla <p.kalverla@esciencecenter.nl>
2024-08-20 12:19:40 +0200

    Make it work

--- wrf-runner/wrf_runner.sh
+++ wrf-runner/wrf_runner.sh
@@ -12,7 +12,12 @@ set -euxo pipefail
 # Load dependencies
 module load 2023
 module load netCDF-Fortran/4.6.1-gompi-2023a  # also loads gcc and gompi
-module load Python/3.11.3-GCCcore-12.3.0
+export NETCDF=$(nf-config --prefix)
+
+# Configure OpenMP threads & core affinity
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
 
 # Set some paths
 export NETCDF=/sw/arch/RHEL8/EB_production/2023/software/netCDF-Fortran/4.6.1-gompi-2023a
@@ -25,12 +30,16 @@ export DATA_HOME=/projects/0/prjs0914/wrf-data/default
 export WPS_HOME=$HOME/wrf-model/WPS
 export WRF_HOME=$HOME/wrf-model/WRF
 
-# Define experiment name
+# Define experiment name; set here or pass in through command line
+# Options: USGS, MODIS_LCZ, WUR, WUR_LCZ
 EXP=USGS
+EXP="${1:-USGS}"
 
 # Make new experiment directory
 export RUNDIR=wrf_experiments/${EXP}
 mkdir -p $RUNDIR
+mkdir -p $RUNDIR/geogrid
+mkdir -p $RUNDIR/metgrid
 
 # Copy experiment-dependent files
 cp namelist.wps_$EXP $RUNDIR/namelist.wps
@@ -47,10 +56,12 @@ cp $WRF_HOME/run/RRTMG* .
 cp $WRF_HOME/run/*.TBL .
 
 # Run experiments
-$WPS_HOME/geogrid.exe
 $WPS_HOME/link_grib.csh "${DATA_HOME}/real-time/july2019/*"
 $WPS_HOME/ungrib.exe
+$WPS_HOME/geogrid.exe
+grep "ERROR" geogrid.log && echo "Aborting: ERROR in geogrid.log." && exit 1
 $WPS_HOME/metgrid.exe
+grep "ERROR" metgrid.log && echo "Aborting: ERROR in metgrid.log." && exit 1
 
 $WRF_HOME/real.exe
-# $WRF_HOME/wrf.exe
+# srun $WRF_HOME/wrf.exe
